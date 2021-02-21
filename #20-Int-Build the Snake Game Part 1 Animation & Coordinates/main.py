3f6b9f550a9ac3db26240c50549dc1c1d1c34c0c
Vitor Malta <vitorcamalta@gmail.com>
2021-02-21 15:58:31 -0300

    Refeito o corpo da aranha

--- #20-Int-Build the Snake Game Part 1 Animation & Coordinates/main.py
+++ #20-Int-Build the Snake Game Part 1 Animation & Coordinates/main.py
@@ -1,47 +1,15 @@
 from turtle import Turtle, Screen
 
 screen = Screen()
-screen.setup(height=600, width=600)
+screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.listen()
 
-snake_body = []
-snake_position = [0, 20]  # initial position
-for i in range(0, 3):
-    snake_body.append(Turtle(shape="square",))
-    snake_body[i].penup()
-    snake_body[i].color("white")
-    snake_body[i].goto(snake_position[0], (snake_position[1]))
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-    snake_position[0] -= 20
-
-
-def move_forward():
-    for index in range(0, len(snake_body)):
-        snake_body[index].forward(10)
-
-
-def move_backward():
-    snake.backward(20)
-
-
-def rotate_clockwise():
-    snake.right(10)
-
-
-def rotate_counter_clockwise():
-    snake.left(10)
-
-
-def clear_screen():
-    screen.resetscreen()
-
-
-screen.onkey(key="w", fun=move_forward)
-screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="d", fun=rotate_clockwise)
-screen.onkey(key="a", fun=rotate_counter_clockwise)
-screen.onkey(key="c", fun=clear_screen)
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
