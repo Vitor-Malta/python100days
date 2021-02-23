442e6ee4532ef533d8c0b90a5876c5c0f00223ec
Vitor Malta <51498193+Vitor-Malta@users.noreply.github.com>
2021-02-23 07:07:34 -0300

    Classe snake refatorada e adicionado movimento

--- #20-Int-Build the Snake Game Part 1 Animation & Coordinates/main.py
+++ #20-Int-Build the Snake Game Part 1 Animation & Coordinates/main.py
@@ -1,8 +1,28 @@
+from turtle import Screen
 from snake import Snake
-from snake_screen import SnakeScreen
+import time
 
-screen = SnakeScreen()
-screen.run()
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
 snake = Snake()
-snake.move()
+# food = Food()
 
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
